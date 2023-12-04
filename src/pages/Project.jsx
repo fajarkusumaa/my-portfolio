d0eeaff763b4c0a81dd78fc112297436351a71b5
fajarkusumaa <fajarkusuma46@gmail.com>
2023-12-04 16:06:07 +0700

    oke

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,13 @@
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <section className="h-screen w-full bg-slate-50 p-8 pt-32">
+      <div className="">
+        {" "}
+        <h1 className="title text-2xl font-bold text-gray-700">Project</h1>
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Project;
